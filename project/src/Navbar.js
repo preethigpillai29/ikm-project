53f7188d0140557b65bc53e96839a7619be2ac37
preethi <preethigpillai29@gmail.com>
2023-02-25 17:48:54 +0530

    updated code

--- project/src/Navbar.js
+++ project/src/Navbar.js
@@ -232,16 +232,6 @@ export default function PrimarySearchAppBar() {
               </Typography>
             </IconButton>
           </Box>
-
-          {/* <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search> */}
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <IconButton
